a80f7b93735ae55b1093f387d922fbf89450bcf9
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2026-03-15 17:06:34 +0900

    Improve zsh completion and bootstrap module backups

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -4,11 +4,17 @@ compinit -d "$HOME/.zcompdump"
 zstyle ':completion:*' menu select
 zstyle ':completion:*' use-cache yes
 zstyle ':completion:*' cache-path "$HOME/.zsh/cache"
+zstyle ':completion:*' completer _extensions _complete _approximate
+zstyle ':completion:*' complete-options true
 zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' verbose yes
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 zstyle ':completion:*:descriptions' format '%F{yellow}-- %d --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
 zstyle ':completion:*:warnings' format '%F{red}no matches found%f'
 zstyle ':completion:*' group-name ''
+zstyle ':completion:*' special-dirs true
+zstyle ':completion:*' rehash true
 zstyle ':completion:*' squeeze-slashes true
 
 autoload -U select-word-style
